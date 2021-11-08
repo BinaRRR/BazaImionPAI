aff16ea9d9d27657711a3f56cef21c3df3bbbc74
BinaRRR <70214208+BinaRRR@users.noreply.github.com>
2021-11-08 15:56:34 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var n = prompt("Podaj liczbę imion: ");
-var tab = [];
-for (let i = 0; i < n; i++)
-    tab[i] = prompt("Podaj imię numer " +  (i + 1));
-for (let i = 0; i < n; i++)
-    document.write(tab[i] + "<br>");
